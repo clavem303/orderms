b6e6850fa030bfc07ccc7fb0636eff1a05b47dd7
Marcondes Cunha Araujo <clavem303@gmail.com>
2025-06-28 21:41:35 -0300

    Mapping entity in OrderEntity.java and OrderItem.java.

---
+++ src/main/java/br/com/clavem303/btgpactual/orderms/entity/OrderEntity.java
@@ -0,0 +1,59 @@
+package br.com.clavem303.btgpactual.orderms.entity;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Document(collection = "td_orders")
+public class OrderEntity {
+
+    @MongoId
+    private Long orderId;
+
+    @Indexed(name = "customer_id_index")
+    private Long customerId;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal totalPrice;
+    private List<OrderItem> items;
+
+    public OrderEntity() {
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+}
